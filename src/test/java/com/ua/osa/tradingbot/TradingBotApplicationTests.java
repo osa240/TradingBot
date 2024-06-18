b9407dc38592f972ee8d9e0b76343c9b35086902
s.orlov <orlov30@gmail.com>
2024-06-19 01:22:49 +0300

    first commit

---
+++ src/test/java/com/ua/osa/tradingbot/TradingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.osa.tradingbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

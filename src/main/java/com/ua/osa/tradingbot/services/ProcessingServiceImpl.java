f12dc0d0c713fdc295b8abd2cbbd16f1ea9fb545
s.orlov <orlov30@gmail.com>
2024-06-30 19:39:29 +0300

    changes in processing service

--- src/main/java/com/ua/osa/tradingbot/services/ProcessingServiceImpl.java
+++ src/main/java/com/ua/osa/tradingbot/services/ProcessingServiceImpl.java
@@ -1,45 +1,43 @@
 package com.ua.osa.tradingbot.services;
 
-import com.ua.osa.tradingbot.AppProperties;
 import com.ua.osa.tradingbot.models.dto.enums.TradePair;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.ta4j.core.BarSeries;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class ProcessingServiceImpl implements ProcessingService {
-    // TODO: 24.06.2024 for TESTS!
-    private static TradePair pair = TradePair.BTC_USDT;
+    private static final TradePair pair = TradePair.DBTC_DUSDT;
 
-    private final List<BigDecimal> prices = Collections.synchronizedList(new ArrayList<>());
+    private final AtomicReference<BigDecimal> price = new AtomicReference<>(BigDecimal.ZERO);
     private final DecisionService decisionService;
 
     @Override
     public void processingLastPrice(BigDecimal price) {
-        this.prices.add(price);
-
-        if (this.prices.size() > 20) {
-            decisionService.makeDecisionByPriceHistory(this.prices, pair);
-            this.prices.remove(0);
-        } else {
-            log.warn("Not enough data. Size of prices list is: {}, but not 20", prices.size());
-        }
+//        this.prices.add(price);
+//
+//        if (this.prices.size() > 20) {
+//            decisionService.makeDecisionByPriceHistory(this.prices, pair);
+//            this.prices.remove(0);
+//        } else {
+//            log.warn("Not enough data. Size of prices list is: {}, but not 20", prices.size());
+//        }
+        this.price.set(price);
     }
 
     @Override
     public void processingKlains(BarSeries series) {
+        price.set(BigDecimal.valueOf(series.getLastBar().getClosePrice().doubleValue()));
         int size = series.getBarData().size();
-        if (size > 30) {
-            decisionService.makeDecisionByIndicators(series, prices.getLast(), AppProperties.TRADE_PAIR.get());
+        if (size > 26) {
+            decisionService.makeDecisionByIndicators(series);
         } else {
-            log.warn("Not enough data. Size of bars are: {}, but not 30", size);
+            log.warn("Not enough data. Size of bars are: {}, but not 26", size);
         }
     }
 }
